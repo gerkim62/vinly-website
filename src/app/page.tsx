a3ff89de64861eb96236a3b42726b3170f67effd
GERISON KIMATHI <gerkim62@gmail.com>
2024-09-08 16:59:47 +0300

    update seo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -70,9 +70,9 @@ const AboutUsSection = () => (
             <Briefcase className="w-12 h-12 text-indigo-700 mb-4" />
             <h3 className="text-2xl font-semibold mb-4">Our Story</h3>
             <p className="text-gray-700">
-              Welcome to Lyvin Forward Premier Company, a pioneering enterprise
-              in Nairobi, Kenya. We deliver creative solutions that cater to the
-              evolving needs of our clients.
+              Welcome to VINLY FORWARD BRIDGE. Company Limited , a pioneering
+              enterprise in Nairobi, Kenya. We deliver creative solutions that
+              cater to the evolving needs of our clients.
             </p>
           </div>
           <div className="bg-white p-6 rounded-lg shadow-md hover:shadow-xl transition duration-300">
